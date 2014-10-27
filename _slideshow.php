28d416d1f4218774b9d5b95689b03f57b9906606
Mikehannam <hannammichael1997@yahoo.com>
2014-10-27 19:02:01 +0000

    website upload
    first upload (old website)

---
+++ _slideshow.php
@@ -0,0 +1,13 @@
+		<div id="slideshow">
+            <div>
+                 <img src="imgs/rotate1.jpg" alt="Image 1">
+            </div>
+            <div>
+                 <img src="imgs/rotate2.jpg" alt="Image 2">
+            </div>
+            <div>
+                 <img src="imgs/rotate3.jpg" alt="Image 3">
+            </div>
+   		</div>
+        
+        <div style="clear:both; height:0px;"></div>
